d9dddf00613044635b2b9a588fe08a6246f64e16
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-03-15 14:09:53 -0400

    customer full functionality

--- Bamazon-Customer/bamazonCustomer.js
+++ Bamazon-Customer/bamazonCustomer.js
@@ -14,10 +14,10 @@ console.log('connected as id ' + connection.threadId);
 connection.query("SELECT * FROM products", function(err,res){
     if(err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log("-----------------------------------");
-      console.log("ID: " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name+ " | " + res[i].price + " | " + res[i].stock_quantity);
+      console.log("-----------------------------------------------------------");
+      console.log("ID: " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name+ " | Price: $" + res[i].price + " | Stock: " + res[i].stock_quantity);
       }
-      console.log("-----------------------------------");
+      console.log("-----------------------------------------------------------");
       inquirer.prompt([
         {
             type: "input",
@@ -30,7 +30,7 @@ connection.query("SELECT * FROM products", function(err,res){
             name: "quantity"
         }]).then(function(data){
             purchase(data.purchase, data.quantity)
-            connection.end();
+            // connection.end();
         })
 })
 function purchase(x,y){
@@ -38,8 +38,26 @@ function purchase(x,y){
         var totalcost = y*result[0].price;
         if(y>result[0].stock_quantity){
             console.log("We do not have enough stock to fulfill your order")
+            connection.end();
         }else{
-            
-            console.log("the cost of " + y + " " + result[0].product_name + "(s) is: $" + totalcost)};
-    })
+            var remainingStock =result[0].stock_quantity - y
+            console.log("the cost of " + y + " " + result[0].product_name + "(s) is: $" + totalcost)
+            update(x,remainingStock)
+        };
+        })
 }
+function update(x,y){
+    connection.query("UPDATE products SET ? WHERE ?", 
+    [
+        {
+            stock_quantity: y
+        },
+        {
+            item_id: x
+        }
+    ], function(err, result){
+        if(err) throw err;
+        console.log(result.affectedRows + " products updated!");
+        connection.end();
+    }
+)}
